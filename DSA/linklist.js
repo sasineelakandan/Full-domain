071ae39d337fc0b5f28b1b4d98be5caea499d1e9
sasineelakandan <devasasi887@gmail.com>
2024-09-02 14:33:47 +0530

    linked list

---
+++ DSA/linklist.js
@@ -0,0 +1,79 @@
+class Node{
+  constructor(val){
+    this.val=val
+    this.next=null
+  }
+}
+
+
+class linkedlist{
+  constructor(){
+    this.head=null
+    this.tail=null
+    this.size=0
+  }
+
+  append(val){
+    let node=new Node(val)
+    if(this.size>0){
+      this.tail.next=node
+      this.tail=node
+    }else{
+      this.head=node
+      this.tail=node
+    }
+    this.size++
+  }
+
+  delete(val){
+    let curr=this.head
+    let index=0
+
+    while(curr!==null&&curr.next!==null){
+
+      if (curr.next.val === val) {
+                        
+         curr.next = curr.next.next;
+         this.size--;
+         } else {
+          curr = curr.next;
+        }
+      
+    }
+    
+  }
+
+  reverse(){
+    let curr=this.head
+    let prev=null
+    while(curr!==null){
+     let temp=curr.next
+     curr.next=prev
+     prev=curr
+     curr=temp
+    }
+    this.head=prev
+  }
+}
+
+const list=new linkedlist()
+
+list.append(10)
+list.append(20)
+list.append(30)
+list.append(40)
+list.append(50)
+list.delete(40)
+list.reverse()
+console.log(list.head)
+
+
+
+
+
+
+
+
+
+
+
