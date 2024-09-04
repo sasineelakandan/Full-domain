f4b30eb4e4f67ffd4d8960a724f6fa5c1a4611f5
sasineelakandan <devasasi887@gmail.com>
2024-09-04 09:52:34 +0530

    question obj

--- js/modifiedstr.js
+++ js/modifiedstr.js
@@ -1,5 +1,16 @@
-const str='hello world'
-let b=str.slice(1)
-console.log(b)
-console.log(str.charAt(0).toUpperCase()+b)
+// const str='hello world'
+// let b=str.slice(1)
+// console.log(b)
+// console.log(str.charAt(0).toUpperCase()+b)
 
+const obj = {a:[5,3,4,8,6],b:[6,7,2],c:[86,42,35,76,15,86],d:[1]}
+let sum=0
+for(let i in obj){
+    if(obj[i].length>3){
+        let a=obj[i]
+        for(let j=0;j<a.length;j++){
+            sum+=a[j] 
+        }
+    }
+}
+console.log(sum)
