62d4561d53c5feeb4162bcf8e9eccdc43c497430
sasineelakandan <devasasi887@gmail.com>
2025-01-21 17:35:56 +0530

    node prctise start

--- DSA/linklist.js
+++ DSA/linklist.js
@@ -1,69 +1,75 @@
 class Node{
   constructor(val){
-    this.val=val
-    this.next=null
-    
+      this.val=val
+      this.next=null
   }
 }
-
-
 
 class linkedlist{
   constructor(){
-    this.head=null
-    this.next=null
-    this.size=0
+      this.head=null;
+      this.tail=null;
+      this.size=0;
   }
 
-  append(val){
-    let node=new Node(val)
-    if(this.size>0){
-      this.tail.next=node
-      this.tail=node
-    }else{
+ append(val){
+  let node=new Node(val)
+  if(this.size==0){
       this.head=node
-      this.tail=node
-    }
-    
-    this.size++
+  }else{
+      this.tail.next=node
   }
-  deleteVal(val){
-    let current = this.head;
-            while (current !== null && current.next !== null) {
-                if (current.next.val === val) {
-                    current.next = current.next.next;
-                    this.size--;
-                } else {
-                    current = current.next;
-                }
-            }
-            if(this.tail!==null&&this.tail.next!==null){
-                this.tail=current
-            }
-}
-
-reverse(){
+  this.tail=node
+  this.size++
+ }
+ prepend(val){
+  let node=new Node(val)
+  if(this.size==0){
+      this.tail=node
+  }else{
+      node.next=this.head
+  }
+  this.head=node
+  this.size++
+ }
+ reverse(){
   let curr=this.head
   let prev=null
   while(curr!=null){
-    let temp=curr.next
-    curr.next=prev
-    prev=curr
-    curr=temp
+      let temp=curr.next
+      curr.next=prev
+      prev=curr
+      curr=temp
   }
-
   this.head=prev
+ }
+ mid(){
+  let slow=this.head
+  let fast=this.head
+  while(slow!=null&&fast.next!=null){
+    slow=slow.next
+    fast=fast.next.next
+  }
+  console.log(slow.val)
+ }
+ delete(val){
+  let curr=this.head
+  while(curr.next){
+      if(curr.next.val==val){
+          curr.next=curr.next.next
+      }
+      
+      curr=curr.next
+  }
+  this.size--
+ }
 }
-}
-
-
 
 const list=new linkedlist()
-
-list.append(10)
-list.append(20)
-list.append(30)
-list.append(40)
-list.append(50)
-list.reverse()
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+list.prepend(40)
+list.prepend(50)
+list.delete(30)
 console.log(JSON.stringify(list.head))
--- Node/fs.js
+++ Node/fs.js
@@ -1,18 +1,111 @@
-const express = require("express")
+// const express = require("express")
 const fs = require("fs")
-let app = express()
+// let app = express()
 
-app.get("/",(req,res)=>{
-    let i = 0
-    let int = setInterval(()=>{
-     console.log(i)
-    if(i==10){
-        clearInterval(int)
-        console.log("done")
+// app.get("/",(req,res)=>{
+//     let i = 0
+//     let int = setInterval(()=>{
+//      console.log(i)
+//     if(i==10){
+//         clearInterval(int)
+//         console.log("done")
+//     }
+//      fs.appendFileSync("text.text",`${new Date()}`+'\n')
+//      i++
+//  },1000)
+// }).listen(3000,()=>{
+//     console.log("server starts at 3000")
+// })
+
+// const express=require('express')
+// const fs=require('fs')
+// const app=express()
+
+// app.get('/',(req,res)=>{
+
+//      fs.appendFileSync('file.txt',`${new Date()}`)
+//      fs.unlink('file.txt')
+//      res.send('hai')
+// })
+// app.get('/:id',(req,res)=>{
+//     console.log(req.params.id)
+// })
+
+// app.listen(5000,()=>{console.log('server start')});
+
+
+
+const express=require('express')
+const app=express()
+const router=express.Router()
+
+
+// app.use((req,res,next)=>{
+//     console.log('hai')
+//     next()
+// })
+
+// router.use((req,res,next)=>{
+//     console.log('bye')
+//     next()
+// })
+
+
+
+app.get('/',(req,res)=>{
+    let a=Number(req.query.a)+Number(req.query.b)
+    let b= a.toString()
+  fs.writeFile('file.txt',`sum=${b}`,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('completed')
     }
-     fs.appendFileSync("text.text",`${new Date()}`+'\n')
-     i++
- },1000)
-}).listen(3000,()=>{
-    console.log("server starts at 3000")
+  })
+  
+    res.send('hello world')
 })
+
+// router.get('/sasi',(req,res)=>{
+//     res.send('deva')
+// })
+// app.use('/',router)
+
+app.listen(5000,()=>{
+    console.log('server start')
+})
+
+
+
+
+// const emitter=new event()
+
+
+// emitter.on('greet',(name)=>{
+//     console.log(name)
+// })
+
+// emitter.emit('greet','sasi')
+
+// let i=0
+// const a=setInterval(()=>{
+//   if(i==10){
+//     clearInterval(a)
+//   }
+//   fs.appendFileSync('file.txt',`${new Date()}`+'\n')
+//   i++
+// },1000)
+
+// fs.unlink('file.txt',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log('complete');
+        
+//     }
+// })
+
+
+
+
+
